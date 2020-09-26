6cbf447ad10231d7d32da66bf4e77a4bc5214616
Henok Tilahun <henok.299312@nv.ccsd.net>
2020-09-26 17:02:01 +0000

    This is how you save stuff right?

--- src/main/java/ConstructionTester.java
+++ src/main/java/ConstructionTester.java
@@ -4,6 +4,16 @@ public class ConstructionTester
 {
     public static void main(String[] args)
     {
-        // Start here!
+        Scanner in = new Scanner(System.in);
+        double lumberCost = 8;
+        double windowCost = 11;
+        double taxRate, total;
+        int boards, windows;
+
+        System.out.println("Enter the sales tax rate: ");
+        taxRate = in.nextDouble();
+        System.out.println("Enter how many boards you need");
+        
+
     }
 }
